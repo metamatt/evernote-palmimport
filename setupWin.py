d6c66d8b1fbed907c3f1b2061e6900e1637815f4
Matt Ginzton <matt@ginzton.net>
2012-10-24 17:39:36 -0700

    Windows setup improvements.
    Find evernote SDK in new location, instead of requiring it be globally
    installed.
    
    Bundle dependencies into main .exe, instead of separate zipfile which
    people might delete or move or try to unzip.

--- setupWin.py
+++ setupWin.py
@@ -7,28 +7,19 @@ Usage:
 
 from distutils.core import setup
 import py2exe
+import sys
+sys.path.append('./evernote-sdk-python/lib')
 
 APP = ['EvernotePalmMemoImporter.py']
-
-# I haven't figured out how to get this to find the dependent libraries in ./libs, via any of the following:
-#
-#   DATA_FILES = ['en_palmimport.py', 'EvernoteManager.py', 'PalmDesktopNoteParser.py', 'lib']
-#   PACKAGES = ['evernote.edam.notestore.NoteStore', 'evernote.edam.type.ttypes', 'evernote.edam.userstore.UserStore', 'evernote.edam.userstore.constants', 'thrift.protocol.TBinaryProtocol', 'thrift.transport.THttpClient']
-#   PACKAGES = ['lib']
-#
-# It works fine without any of that if the Evernote and Thrift librares are in the Python install directory.
 
 OPTIONS = {
             "dll_excludes" : ["MSVCP90.dll"],                   # Force people to get this themselves
             "dist_dir" : "dist/EvernotePalmMemoImporter-Win32", # Keep files separate from the final dist directory
-            "bundle_files" : 1,                                 # Put everything in library.zip
-           	#"packages" : PACKAGES,
-           	#"includes" : ['lib.evernote.edam.type.ttypes'],
+            "bundle_files" : 1                                 # Put dependencies in a single archive (library.zip by default, but see below)
           }
 
 setup(
 	windows = APP,
-    zipfile = "EvernotePalmMemoImporterLibs.zip",               # Except don't call it library.zip 
-	#data_files = DATA_FILES,
+	zipfile = None,                                              # Instead of creating library.zip, bundle dependencies into main .exe
 	options = {'py2exe' : OPTIONS},
 )
